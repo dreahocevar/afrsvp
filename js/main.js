87deea06238649cc35e21f0d5a94ec82e803fe46
Andrea Hocevar <annhocevar@gmail.com>
2026-05-28 20:01:28 +0200

    Update wedding invitation site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("Wedding site loaded");
+const envelopeButton = document.querySelector(".envelope-link");
+const inviteReveal = document.querySelector("#invite-reveal");
+
+if (envelopeButton && inviteReveal) {
+  envelopeButton.addEventListener("click", () => {
+    const isHidden = inviteReveal.hasAttribute("hidden");
+
+    if (isHidden) {
+      inviteReveal.removeAttribute("hidden");
+      envelopeButton.setAttribute("aria-expanded", "true");
+    }
+
+    inviteReveal.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+}
